94f2cf532078f4411b15b193e6e693bf8a157c7f
Jakub Skurek <JakubS@kentico.com>
2023-04-26 09:44:28 +0200

    helpservice compatibility test

--- gulpfile.js
+++ gulpfile.js
@@ -8,29 +8,34 @@ const cheerio = require('cheerio');
 function fixExport(spaceKey)
 {
     const docsPath = path.join(__dirname, "docs")
-
     const fullPath = path.join(docsPath, spaceKey)
 
-    // Lowercase all HTML files
-    fs.readdir(fullPath, (error, files) => {
-        if (error) console.log(error)
-        files.forEach( file => 
-            {
-                console.log(`Processing ... ${file}`);
-                // full lowercase the html filename
-                fs.renameSync(path.join(fullPath, file), path.join(fullPath, file.toLowerCase()));
-
-                const stats = fs.statSync(path.join(fullPath, file.toLowerCase()))
-                if (!stats.isDirectory())
+    if(fs.existsSync(fullPath))
+    {
+        // Lowercase all HTML files
+        fs.readdir(fullPath, (error, files) => {
+            if (error) console.log(error)
+            files.forEach( file => 
                 {
-                    removeInThisSectionPanelAndMacros(path.join(fullPath, file.toLowerCase()));
-                    fixPageLayout(path.join(fullPath, file.toLowerCase()));
-                }
+                    console.log(`Processing ... ${file}`);
+                    const renamedFilePath = path.join(fullPath, file.toLowerCase());
+                    // full lowercase the html filename
+                    fs.renameSync(path.join(fullPath, file), renamedFilePath);
+
+                    const stats = fs.statSync(renamedFilePath)
+                    if (!stats.isDirectory())
+                    {
+                        //removeInThisSectionPanel(renamedFilePath);
+                        //fixRelatedPagesAndOnThisPagePanels(renamedFilePath);
+
+                        generateHelpServiceRedirectHandler(renamedFilePath);
+                    }
+                })
             })
-        })
+    }
 }
 
-function removeInThisSectionPanelAndMacros(filePath)
+function removeInThisSectionPanel(filePath)
 {
     const html = fs.readFileSync(filePath, 'utf8');
 
@@ -43,76 +48,166 @@ function removeInThisSectionPanelAndMacros(filePath)
     fs.writeFileSync(filePath, $.html());
 }
 
-function fixPageLayout(filePath)
+function fixRelatedPagesAndOnThisPagePanels(filePath)
 {
     const html = fs.readFileSync(filePath, 'utf8');
+    const RIGHTCOLUMN = ".sp-grid-cell.sp-grid-40.sp-grid-sidebar";
+    const RIGHTCOLUMNMARKUP = '<div class="sp-grid-cell sp-grid-40 sp-grid-sidebar"></div>';
+    const LEFTCOLUMN = ".sp-grid-cell.sp-grid-60";
+    const LEFTCOLUMNMARKUP = '<div class="sp-grid-cell sp-grid-60"></div>';
 
     // Parse the HTML using Cheerio
     const $ = cheerio.load(html);
+    // Skip if already processed
+    if ($(LEFTCOLUMN).length) return;
 
     // Find the first occurrence of the HTML code to be wrapped
     const onThisPage = $('.confbox.panel .title.panel-header:contains("On this page")').first().parent();
     const relatedPages = $('.confbox.panel .title.panel-header:contains("Related pages")').first().parent();
-    if (onThisPage.length || relatedPages.length)
+    if (onThisPage.length)
     {
-        $('<div class="section-right"></div>').insertBefore(onThisPage);
-        $('.section-right').append(onThisPage);
-        $('.section-right').append(relatedPages);
+        $(RIGHTCOLUMNMARKUP).insertBefore(onThisPage);
+        $(RIGHTCOLUMN).append(onThisPage);
+        $(RIGHTCOLUMN).append(relatedPages);
+    }
+    else if (relatedPages.length)
+    {
+        $(RIGHTCOLUMNMARKUP).insertBefore(relatedPages);
+        $(RIGHTCOLUMN).append(onThisPage);
+        $(RIGHTCOLUMN).append(relatedPages);
     }
 
     // Find the element with id "main-content"
     const mainContentFirstChild = $('#main-content').children().first();
-    $('<div class="section-left"></div>').insertBefore(mainContentFirstChild);
-    const sectionLeft = $('.section-left');
+    $(LEFTCOLUMNMARKUP).insertBefore(mainContentFirstChild);
+    const sectionLeft = $(LEFTCOLUMN);
 
-    const sectionLeftElements = $('.section-right').prevUntil(sectionLeft);
+    const sectionLeftElements = $(RIGHTCOLUMN).prevUntil(sectionLeft);
     const array = sectionLeftElements.toArray().reverse();
     
     sectionLeft.append(array);
-    // mainContent.children().toArray().reverse().html();
-
-    // const leftSection = mainContent.nextAll();
-    // leftSection.wrap('<div class="section-left"></div>');
-    // Find all elements above the first element up until the element with id "main-content"
-    // const contentAbove = firstElement.prevUntil(mainContent);
-
-    // Wrap the content above the first element in an additional div with the "section-left" class name
-    // contentAbove.wrapAll('<div class="section-left"></div>');
-
-    
-
-    // if (firstElement.length)
-    // {
-    //     const secondElement = firstElement.next('.confbox.panel .title.panel-header:contains("Related pages")').first().parent();
-
-    //     firstElement.insertBefore('<div class="section-right"></div>');
-    //     const rightSection = $('.section-right');
-    //     // If the second element exists, wrap both elements in an additional div with the "section-right" class name
-    //     if (secondElement.length) {            
-            
-    //         rightSection.append(firstElement);
-    //         //firstElement.remove()
-    //         rightSection.append(secondElement);
-    //         //secondElement.remove();
-
-    //     // firstElement.add(secondElement).wrapAll('<div class="section-right"></div>');
-    //     } else {
-    //         rightSection.append(firstElement);
-    //         //firstElement.remove();
-    //     // Otherwise, only wrap the first element
-    //     //firstElement.wrap('<div class="section-right"></div>');
-    //     }
-    // }
-
-    
 
     // Save the modified HTML file
     fs.writeFileSync(filePath, $.html());
 }
 
+// Converts the page id to a byte array for further encoding
+function longToByteArray(long) {
+    var byteArray = [0, 0, 0, 0];
+    for ( var index = 0; index < byteArray.length; index ++ ) {
+      var byte = long & 0xff;
+      byteArray [ index ] = byte;
+      long = (long - byte) / 256 ;
+    }
+    return byteArray;
+};
+
+
+// Encodes provided page id in Base-64
+function pageIdToB64(pageId) {
+    return btoa(
+        longToByteArray(pageId)
+            .map(String.fromCharCode)
+            .map(function (s) {
+                return s[0];
+            })
+            .join("")
+      )
+      .replace(/=/g,"");
+};
+
+
+// Generates a Confluence page identifier from the provided B-64 encoded page id
+// Taken directly from Confluence Server 7.2 source code
+// com.atlassian.confluence.pages.TinyId
+function generateTinyId(btoadId) {
+    var padding = true;
+    var tinyString = "";
+
+    for (var i = btoadId.length - 1; i >= 0 ; i--) {
+
+        var character = btoadId.charAt(i);
+
+        if (character === '=' || character == '\n')
+            continue;
+
+        if (padding && character === 'A')
+            continue;
+
+        padding = false;
+
+        if (character === '/') {
+            tinyString = '-' + tinyString;
+        }
+        else if (character === '+') {
+            tinyString = '_' + tinyString;
+        }
+        else {
+            tinyString = character + tinyString;
+        }
+    }
+
+    if (tinyString.length > 0) {
+        var lastChar = tinyString.charAt(tinyString.length - 1);
+
+        // CONF-9299 some email clients don't like URLs that end with a punctation
+        if (lastChar === '-' || lastChar === '_')
+            tinyString += '/';
+    }
+
+    return tinyString;
+};
+
+function getTinyIdentifier(pageId)
+{
+    return generateTinyId(pageIdToB64(pageId))
+}
+
+function generateHelpServiceRedirectHandler(filePath)
+{
+    const html = fs.readFileSync(filePath, 'utf8');
+
+    // // Parse the HTML using Cheerio
+    const $ = cheerio.load(html);
+
+    const pageId = $(`body`).attr('elementpageid');
+    const tinyId = getTinyIdentifier(pageId);
+
+    console.log(`PageId ${pageId} -> tinyId ${tinyId}`);  
+    const pagePath = filePath.replace(`${__dirname}\\docs`, "")  
+
+    const redirectHandler = `<!DOCTYPE html>
+    <html lang="en-US">
+      <meta charset="utf-8">
+      <title>Redirecting&hellip;</title>
+      <link rel="canonical" href="..${pagePath}">
+      <script>location="..${pagePath}"</script>
+      <meta http-equiv="refresh" content="0; url=..${pagePath}">
+      <meta name="robots" content="noindex">
+      <h1>Redirecting&hellip;</h1>
+      <a href="..${pagePath}">Click here if you are not redirected.</a>
+    </html>`;
+
+    fs.writeFileSync(path.join(__dirname, `docs/x/${tinyId}.html`), redirectHandler);
+}
+
+
+// =========================================================
+// ===================== GULP CALLS ========================
+// =========================================================
+
+
 async function fixK11(cb)
 {
     fixExport("k11");
+    fixExport("k11tutorial");
+    fixExport("k11api");
+    return cb;
+}
+
+async function fixK10(cb)
+{
+    fixExport("k10");
     return cb;
 }
 
