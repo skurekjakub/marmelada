9d8e5c65023ff73f1e3885114d5ec8ec1118444a
Jakub Skurek <JakubS@kentico.com>
2023-06-23 11:54:55 +0200

    delete redirects

---
+++ test.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.writeFileSync(path.join(__dirname, `docs/x/LAkbB.html`), "test bullshit");
+fs.writeFileSync(path.join(__dirname, `docs/x/lAkbB.html`), "test bullshit");
+
+
+fs.writeFileSync(path.join(__dirname, `docs/x/lAkbB.html`), "test bullshit");
+fs.writeFileSync(path.join(__dirname, `docs/x/lakbB.html`), "test bullshit");
